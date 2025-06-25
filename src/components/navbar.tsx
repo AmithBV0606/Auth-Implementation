486c8a5cd2d98b7644c6b478ebbcc04b01f3cdca
Amith B V <amithrao0606@gmail.com>
2025-06-25 19:55:13 +0530

    Customized "/" route page and updated navbar UI.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,10 @@ import { ThemeToggleButton } from "./theme-toggle-button";
 
 export default function Navbar() {
   return (
-    <div className="container mx-auto p-4 border-2 rounded-2xl mt-6">
-      <ThemeToggleButton />
+    <div className="container w-[95%] mx-auto p-4 border-2 rounded-2xl mt-6">
+      <div>
+        <ThemeToggleButton />
+      </div>
     </div>
   );
 }
