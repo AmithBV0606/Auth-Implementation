f517188e41a2a54967ae209bfcb2733c5b55a53c
Amith B V <amithrao0606@gmail.com>
2025-06-27 08:49:21 +0530

    Implemented middlewares and protected "/admin" and "/private" routes.

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -5,13 +5,13 @@ import { Card } from "@/components/ui/card";
 import { getCurrentUser } from "@/auth/nextjs/currentUser";
 
 export default async function PrivatePage() {
-  const currentUser = await getCurrentUser({ redirectIfNotFound: true });
+  const currentUser = await getCurrentUser();
   // const currentUser = { role: "user" };
 
   return (
     <div className="container mx-auto p-4 2xl:ml-[70px] mt-6 w-full">
       <Card className="p-6 w-[80%] sm:w-[50%] lg:w-[25%] h-52">
-        <h1 className="text-4xl mb-8">Private: {currentUser.role}</h1>
+        <h1 className="text-4xl mb-8">Private: {currentUser?.role}</h1>
 
         <div className="flex gap-2">
           <ToggleRoleButton />
---
+++ src/middleware.ts
@@ -0,0 +1,44 @@
+import { asc } from "drizzle-orm";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getUserFromSession } from "./auth/core/session";
+
+const privateRoutes = ["/private"];
+const adminRoutes = ["/admin"];
+
+async function middlewareAuth(request: NextRequest) {
+  // To Check if we're on private or admin route :
+  if (privateRoutes.includes(request.nextUrl.pathname)) {
+    const user = await getUserFromSession(request.cookies);
+    // If there is no cookies or user :
+    if (user == null) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+  }
+
+  if (adminRoutes.includes(request.nextUrl.pathname)) {
+    const user = await getUserFromSession(request.cookies);
+    // If there is no cookies or user :
+    if (user == null) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    if (user.role != "admin") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+}
+
+export async function middleware(request: NextRequest) {
+  const response = (await middlewareAuth(request)) ?? NextResponse.next();
+  return response;
+}
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
