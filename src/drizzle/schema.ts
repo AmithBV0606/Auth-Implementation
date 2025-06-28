2cbaf66611b7674022add9b710f926955c39ffd0
Amith B V <amithrao0606@gmail.com>
2025-06-28 07:09:04 +0530

    Created new DB schemas for adding OAuth information.

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,8 +1,10 @@
+import { relations } from "drizzle-orm";
 import {
   pgEnum,
   pgTable,
   text,
   timestamp,
+  primaryKey,
   uuid,
 } from "drizzle-orm/pg-core";
 
@@ -23,3 +25,38 @@ export const UserTable = pgTable("users", {
     .defaultNow()
     .$onUpdate(() => new Date()),
 });
+
+export const oAuthProviders = ["discord", "github"] as const;
+export type OAuthProvider = (typeof oAuthProviders)[number];
+export const OAuthProviderEnum = pgEnum("oauth_provider", oAuthProviders);
+
+export const UserOAuthAccountTable = pgTable(
+  "user_oauth_accounts",
+  {
+    userId: uuid()
+      .notNull()
+      .references(() => UserTable.id, { onDelete: "cascade" }),
+    provider: OAuthProviderEnum().notNull(),
+    providerAccountId: text().notNull().unique(),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true })
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+  },
+  (t) => [primaryKey({ columns: [t.providerAccountId, t.provider] })]
+);
+
+export const userRelation = relations(UserTable, ({ many }) => ({
+  oAuthAccounts: many(UserOAuthAccountTable),
+}));
+
+export const UserOAuthAccountRelationships = relations(
+  UserOAuthAccountTable,
+  ({ one }) => ({
+    user: one(UserTable, {
+      fields: [UserOAuthAccountTable.userId],
+      references: [UserTable.id],
+    }),
+  })
+);
